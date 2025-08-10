ea00dca9d0147052dc25ac28c0d882d61adb97d1
The-FerryMan1 <mercedroxy@gmail.com>
2025-08-10 20:33:40 +0800

    second commit

---
+++ app/components/sideProfile.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <aside class="w-1/5  rounded-md">
+        <section class=" flex justify-center ">
+            <img src="https://www.kindpng.com/picc/m/722-7221920_placeholder-profile-image-placeholder-png-transparent-png.png"
+                alt="" class="rounded-full aspect-auto p-8 w-3/4 ">
+        </section>
+
+        <section class="p-10 font-rubik-mono flex flex-col gap-6 justify-start items-start text-xl">
+            <div class="flex gap-3 justify-between items-center">
+                <h1 class="text-xl">The-FerryMan1</h1>
+            </div>
+
+            <div class="flex gap-3 justify-between items-center">
+                <Icon name="i-lucide-map-pin" size="20" />
+                <p class="font-rubik ">Philippines</p>
+            </div>
+
+            <div class="flex flex-col gap-3 justify-between items-center">
+
+                <p class="text-lg">Contacts</p>
+                <div class="flex justify-start items-start w-full gap-5">
+                    <a href="https://github.com/The-FerryMan1" class="font-rubik group py-2 ">
+                        <Icon name="i-lucide-github" size="30"
+                            class="group-hover:scale-120 transition-transform duration-150" />
+                    </a>
+                    <a href="https://mail.google.com/mail/u/0/?to=mercedroxy@gmail.com&fs=1&tf=cm"
+                        class="font-rubik group py-2">
+                        <Icon name="i-lucide-mail" size="30"
+                            class="group-hover:scale-120 transition-transform duration-150" />
+                    </a>
+                </div>
+
+            </div>
+
+        </section>
+    </aside>
+</template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,41 +3,59 @@
 
 <template>
     <div class="h-screen flex gap-4  bg-slate-950 p-5 overflow-hidden text-white">
-        <aside class="w-1/5  rounded-md">
-            <section class=" flex justify-center ">
-                <img src="https://www.kindpng.com/picc/m/722-7221920_placeholder-profile-image-placeholder-png-transparent-png.png"
-                    alt="" class="rounded-full aspect-auto p-8 w-3/4 ">
+        <SideProfile />
+
+
+        <main class="grow w-full rounded-md overflow-x-hidden overflow-y-auto">
+
+            <!-- Role -->
+            <section class="py-10 px-2 ">
+                <h1 class="font-rubik-mono text-7xl text-wrap">
+                    <span class="text-black text-shadow-md text-shadow-white text-shadow-">Full-Stack </span>
+                    Web Devopler
+                </h1>
             </section>
-
-            <section class="p-10 font-rubik-mono flex flex-col gap-6 justify-start items-start text-xl">
-                <div class="flex gap-3 justify-between items-center">
-                    <h1 class="text-xl">The-FerryMan1</h1>
+            <!-- Tech Stack -->
+            <section class="w-full">
+                <div class="relative flex overflow-x">
+                    <div class="animate-marquee py-12 whitespace-nowrap flex gap-30 font-rubik-mono text-lg">
+                        <span class="mx-4 text-4xl flex gap-4 justify-start items-center">
+                            <img src="../assets/img/Nuxt_logo.png" alt="" class="size-18 aspect-auto">
+                            <p>NUXT</p>
+                        </span>
+                        <span class="mx-4 text-4xl  flex gap-4 justify-start items-center ">
+                            <img src="../assets/img/HTML5_logo_and_wordmark.png" alt="" class="size-18 aspect-auto">
+                            HTML</span>
+                        <span class="mx-4 text-4xl  flex gap-4 justify-start items-center">
+                            <img src="../assets/img/Official_css_logo.png" alt="" class="size-18 aspect-auto">
+                            CSS
+                        </span>
+                        <span class="mx-4 text-4xl  flex gap-4 justify-start items-center">
+                            <img src="../assets/img/tailwindcss-mark.svg" alt="" class="size-18 aspect-auto">
+                            TAILWIND
+                        </span>
+                        <span class="mx-4 text-4xl  flex gap-4 justify-start items-center">
+                            <img src="../assets/img/vue.svg" alt="" class="size-18 aspect-auto">
+                            VUE
+                        </span>
+                        <span class="mx-4 text-4xl  flex gap-4 justify-start items-center">
+                            <img src="../assets/img/hono.svg" alt="" class="size-18 aspect-auto">
+                            Hono
+                        </span>
+                        <span class="mx-4 text-4xl  flex gap-4 justify-start items-center">
+                            <img src="../assets/img/Laravel.png" alt="" class="size-18 aspect-auto">
+                            LARAVEL
+                        </span>
+                        <span class="mx-4 text-4xl  flex gap-4 justify-start items-center">
+                            <img src="../assets/img/Typescript.png" alt="" class="size-18 aspect-auto">
+                            TYPESCRIPT
+                        </span>
+                    </div>
                 </div>
-
-                <div class="flex gap-3 justify-between items-center">
-                    <Icon name="i-lucide-map-pin" size="20" />
-                    <p class="font-rubik ">Philippines</p>
-                </div>
-
-                <div class="flex gap-3 justify-between items-center">
-                    <a href="https://github.com/The-FerryMan1" class="font-rubik">
-                        <Icon name="i-lucide-github" size="30" />
-                    </a>
-                    <a href="https://mail.google.com/mail/u/0/?to=mercedroxy@gmail.com&fs=1&tf=cm" class="font-rubik">
-                        <Icon name="i-lucide-mail" size="30"  />
-                    </a>
-                </div>
-
             </section>
-
-            <section>
-
-            </section>
-        </aside>
-
-        <main class="grow  rounded-md ">
-            body
         </main>
+
+
         <SideNav />
 
     </div>
