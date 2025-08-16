2fb29d92f1af13650c8da4d0eaeb1827952f7ba4
The-FerryMan1 <mercedroxy@gmail.com>
2025-08-16 11:07:49 +0800

    fixing img not able to build

--- app/components/techStack.vue
+++ app/components/techStack.vue
@@ -4,8 +4,10 @@
 
 <template>
     <section class="w-full flex justify-center item-center ">
-        <div class="relative gap-10 justify-between font-rubik-mono text-lg h-[80px] p-2 max-w-5xl  w-full flex overflow-hidden ">
-            <a href="https://nuxt.com/" class="  mx-10 flex gap-2 items-center item item1 hover:scale-75 transition-transform duration-150">
+        <div
+            class="relative gap-10 justify-between font-rubik-mono text-lg h-[80px] p-2 max-w-5xl  w-full flex overflow-hidden ">
+            <a href="https://nuxt.com/"
+                class="  mx-10 flex gap-2 items-center item item1 hover:scale-75 transition-transform duration-150">
                 <img src="../assets/img/Nuxt_logo.png" alt="" class="size-12 object-contain">
                 <p>NUXT</p>
             </a>
@@ -16,26 +18,31 @@
             </a>
             <a href="https://developer.mozilla.org/en-US/docs/Web/CSS"
                 class="    mx-2 flex gap-2 items-center item item3 hover:scale-75 transition-transform duration-150">
-                <img src="../assets/img/Official_css_logo.png" alt="" class="size-12  object-contain">
+                <img src="../assets/img/cssLogo.png" alt="" class="size-12  object-contain">
                 CSS
             </a>
-            <a href="https://tailwindcss.com/" class="    mx-2 flex gap-2 items-center item item4 hover:scale-75 transition-transform duration-150">
+            <a href="https://tailwindcss.com/"
+                class="    mx-2 flex gap-2 items-center item item4 hover:scale-75 transition-transform duration-150">
                 <img src="../assets/img/tailwindcss.svg" alt="" class="size-12 object-contain">
                 TAILWIND
             </a>
-            <a href="https://vuejs.org/" class="    mx-2 flex gap-2 items-center item item5 hover:scale-75 transition-transform duration-150">
+            <a href="https://vuejs.org/"
+                class="    mx-2 flex gap-2 items-center item item5 hover:scale-75 transition-transform duration-150">
                 <img src="../assets/img/vue.svg" alt="" class="size-12 object-contain">
                 VUE
             </a>
-            <a href="https://hono.dev/" class="    mx-2 flex gap-2 items-center item item6 hover:scale-75 transition-transform duration-150">
+            <a href="https://hono.dev/"
+                class="    mx-2 flex gap-2 items-center item item6 hover:scale-75 transition-transform duration-150">
                 <img src="../assets/img/hono.svg" alt="" class="size-12 object-contain">
                 Hono
             </a>
-            <a href="https://laravel.com/" class="    mx-2 flex gap-2 items-center item item7 hover:scale-75 transition-transform duration-150">
+            <a href="https://laravel.com/"
+                class="    mx-2 flex gap-2 items-center item item7 hover:scale-75 transition-transform duration-150">
                 <img src="../assets/img/Laravel.png" alt="" class="size-12 object-contain">
                 LARAVEL
             </a>
-            <a href="https://www.typescriptlang.org/" class="    mx-2 flex gap-2 items-center item item8 hover:scale-75 transition-transform duration-150">
+            <a href="https://www.typescriptlang.org/"
+                class="    mx-2 flex gap-2 items-center item item8 hover:scale-75 transition-transform duration-150">
                 <img src="../assets/img/Typescript.png" alt="" class="size-12 object-contain">
                 TYPESCRIPT
             </a>
